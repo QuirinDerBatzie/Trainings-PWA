f6c208ed40452e491896bdf2eb7261923c1f68f7
QuirinDerBatzie <q.batzer@gmail.com>
2025-09-06 14:24:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,16 @@
-const CACHE = "training-log-cache-v1";
+const CACHE = "training-log-cache-v3";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
-  "https://cdn.jsdelivr.net/npm/chart.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (e)=>{
-  e.waitUntil(
-    caches.open(CACHE).then(cache=> cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(cache=> cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e)=>{
@@ -25,18 +22,6 @@ self.addEventListener("activate", (e)=>{
 });
 
 self.addEventListener("fetch", (e)=>{
-  const url = new URL(e.request.url);
-  // Network-first für Chart.js CDN, sonst Cache-first
-  if (url.href.includes("cdn.jsdelivr.net")){
-    e.respondWith(
-      fetch(e.request).then(res=>{
-        const copy = res.clone();
-        caches.open(CACHE).then(cache=>cache.put(e.request, copy));
-        return res;
-      }).catch(()=>caches.match(e.request))
-    );
-    return;
-  }
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
